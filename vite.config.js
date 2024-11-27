3d771e89fe91d3316f770a8d1df80b37dffb8b65
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2024-11-27 14:42:10 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.jsx"],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.jsx",
+                "resources/css/filament/admin/theme.css"
+            ],
             refresh: true,
         }),
         react(),
