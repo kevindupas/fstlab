00ee87d63948d39e85e15011ec6f7f5ba2317ae2
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2025-01-06 11:53:33 +0100

    Update 2024_12_22_194232_add_rejected_to_experiment_access_requests_table.php

--- database/migrations/2024_12_22_194232_add_rejected_to_experiment_access_requests_table.php
+++ database/migrations/2024_12_22_194232_add_rejected_to_experiment_access_requests_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,7 +13,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('experiment_access_requests', function (Blueprint $table) {
-            $table->enum('status', ['pending', 'approved', 'rejected', 'revoked'])->default('pending')->change();
+            // $table->enum('status', ['pending', 'approved', 'rejected', 'revoked'])->default('pending')->change();
+            DB::statement("ALTER TYPE experiment_access_request_status ADD VALUE IF NOT EXISTS 'revoked'");
         });
     }
 
