74731c0b16e89ad1cb932b2f930e9cc296f3bfaf
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2025-02-01 20:14:28 +0100

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,16 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- Pour iOS -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
+    <!-- Pour Android -->
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="theme-color" content="#000000">
+
+    <!-- Empêcher le zoom -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Platform d'expérimentation</title>
 
